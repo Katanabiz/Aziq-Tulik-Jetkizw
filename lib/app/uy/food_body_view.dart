32134f7c05110d0579c4c17e0a52b2a28717a476
Samuraimiz <myeskat@gmail.com>
2022-09-15 17:31:54 +0800

    slader maked matrix4

--- lib/app/uy/food_body_view.dart
+++ lib/app/uy/food_body_view.dart
@@ -16,6 +16,8 @@ class _FoodBodyViewState extends State<FoodBodyView> {
   PageController pageController = PageController(viewportFraction: 0.85);
 
   var currentPageValue = 0.0;
+  double scallFactor = 0.8;
+  double height = 220.0;
 
   @override
   void initState() {
@@ -47,75 +49,101 @@ class _FoodBodyViewState extends State<FoodBodyView> {
   }
 
   Widget _pageItemBuild(int index) {
-    return Stack(children: [
-      Container(
-          height: 220,
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven
-                ? const Color(0xff69c5df)
-                : const Color(0xff9294cc),
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/food0.png'),
-            ),
-          )),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-            height: 120,
-            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    Matrix4 matrix = Matrix4.identity();
+
+    if (index == currentPageValue.floor()) {
+      var currentScal = 1 - (currentPageValue - index) * (1 - scallFactor);
+      var currenTransform = height * (1 - currentScal) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScal, 1)
+        ..setTranslationRaw(0, currenTransform, 0);
+    } else if (index == currentPageValue.floor() + 1) {
+      var currentScal =
+          scallFactor + (currentPageValue - index + 1) * (1 - scallFactor);
+      var currenTransform = height * (1 - currentScal) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScal, 1)
+        ..setTranslationRaw(0, currenTransform, 0);
+    } else if (index == currentPageValue.floor() - 1) {
+      var currentScal = 1 - (currentPageValue - index) * (1 - scallFactor);
+      var currenTransform = height * (1 - currentScal) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScal, 1)
+        ..setTranslationRaw(0, currenTransform, 0);
+    } else {
+      var currentScal = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currentScal, 1)
+        ..setTranslationRaw(0, height * (1 - scallFactor) / 2, 1);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(children: [
+        Container(
+            height: 220,
+            margin: const EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30), color: Colors.white),
-            child: Container(
-                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const BigTextWidget(text: 'Malay Side'),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        Wrap(
-                            children: List.generate(5,
-                                // arrow means retun function
-                                (index) {
-                          return const Icon(Icons.star,
-                              size: 20, color: AppTusteri.negizigTus);
-                        })),
-                        const SizedBox(width: 10),
-                        const SmalTextWidget(text: '4.5'),
-                        const SizedBox(width: 10),
-                        const SmalTextWidget(text: '1234'),
-                        const SizedBox(width: 10),
-                        const SmalTextWidget(text: 'Comments')
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: const [
-                        IconsAndTextWidget(
-                            icon: Icons.circle_sharp,
-                            text: 'Normal',
-                            iconColor: AppTusteri.belgisesiTus1),
-                        IconsAndTextWidget(
-                            icon: Icons.location_on,
-                            text: '1.7km',
-                            iconColor: AppTusteri.matinTus),
-                        IconsAndTextWidget(
-                            icon: Icons.access_time_rounded,
-                            text: '32min',
-                            iconColor: AppTusteri.belgisesiTus2),
-                      ],
-                    )
-                  ],
-                ))),
-      )
-    ]);
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven
+                  ? const Color(0xff69c5df)
+                  : const Color(0xff9294cc),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/food0.png'),
+              ),
+            )),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: Container(
+                  padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const BigTextWidget(text: 'Malay Side'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Wrap(
+                              children: List.generate(5,
+                                  // arrow means retun function
+                                  (index) {
+                            return const Icon(Icons.star,
+                                size: 20, color: AppTusteri.negizigTus);
+                          })),
+                          const SizedBox(width: 10),
+                          const SmalTextWidget(text: '4.5'),
+                          const SizedBox(width: 10),
+                          const SmalTextWidget(text: '1234'),
+                          const SizedBox(width: 10),
+                          const SmalTextWidget(text: 'Comments')
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: const [
+                          IconsAndTextWidget(
+                              icon: Icons.circle_sharp,
+                              text: 'Normal',
+                              iconColor: AppTusteri.belgisesiTus1),
+                          IconsAndTextWidget(
+                              icon: Icons.location_on,
+                              text: '1.7km',
+                              iconColor: AppTusteri.matinTus),
+                          IconsAndTextWidget(
+                              icon: Icons.access_time_rounded,
+                              text: '32min',
+                              iconColor: AppTusteri.belgisesiTus2),
+                        ],
+                      )
+                    ],
+                  ))),
+        )
+      ]),
+    );
   }
 }
